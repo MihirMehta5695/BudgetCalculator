4b07ca362377b173dd7df2fae162f78ace67bc6c
Mihir <mihirm5695@gmail.com>
2020-06-30 19:48:39 +0530

    Completed all mocked animation, started with functionality. 1hr 27mins

--- budget-app/src/app/add-item-form/add-item-form.component.ts
+++ budget-app/src/app/add-item-form/add-item-form.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from "@angular/core";
+import { NgForm } from "@angular/forms";
+import { BudgetItem } from "src/shared/models/budget-item.model";
+import { EventEmitter } from "@angular/core";
 
 @Component({
-  selector: 'app-add-item-form',
-  templateUrl: './add-item-form.component.html',
-  styleUrls: ['./add-item-form.component.scss']
+  selector: "app-add-item-form",
+  templateUrl: "./add-item-form.component.html",
+  styleUrls: ["./add-item-form.component.scss"],
 })
 export class AddItemFormComponent implements OnInit {
+  @Input() item: BudgetItem = new BudgetItem("", null);
+  constructor() {}
 
-  constructor() { }
+  @Output formSubmit: EventEmitter<BudgetItem> = new EventEmitter<BudgetItem>();
 
-  ngOnInit() {
+  ngOnInit() {}
+
+  onSubmit(form: NgForm) {
+    this.formSubmit.emit(form.value);
   }
-
 }
